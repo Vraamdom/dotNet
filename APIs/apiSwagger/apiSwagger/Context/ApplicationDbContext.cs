3565eeac495b6defa0a8d00131e3d34c0ff517b6
unknown <Sena@MEDAPRCSGFSP830.FormacionSena.RED>
2024-03-11 11:25:13 -0500

    API CON SWAGGER

---
+++ APIs/apiSwagger/apiSwagger/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using apiSwagger.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiSwagger.Context
+{
+    public class ApplicationDbContext:DbContext
+
+    {
+        public DbSet<Producto> Productos { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ APIs/apiSwagger/apiSwagger/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace apiSwagger.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public int precio { get; set; }
+        public int stock { get; set; }
+
+    }
+}
